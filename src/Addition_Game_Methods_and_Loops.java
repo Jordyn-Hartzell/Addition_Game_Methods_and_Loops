43396d4098773c5bfde70f38f9f1eaf1a7079937
Jordyn Hartzell <hartzellj@student.swosu.edu>
2015-11-09 09:52:24 -0600

    added outline and methods

--- src/Addition_Game_Methods_and_Loops.java
+++ src/Addition_Game_Methods_and_Loops.java
@@ -1,12 +1,38 @@
-
+import java.util.Scanner;
 public class Addition_Game_Methods_and_Loops {
-
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		int score;
+		// Call the addition game
+		additionGameMethod();
+		
+		System.out.print("All done.");
 	}
-
+	public static void additionGameMethod() {
+		
+		int score = 0;
+		int hardness = 5;
+		int stopIndex = 2;
+		boolean isAnswerCorrect;
+		
+		for(int index=0; index<stopIndex; index=index+1){
+			System.out.println("Ahoy");
+			isAnswerCorrect = checkAnswerMethod(hardness);
+			if(isAnswerCorrect){
+				System.out.println("It was correct");
+			}else{
+				System.out.println("It was not correct");
+			}
+		}
+		
+		
+		return;
+		
+		
+		
+	}
+	public static boolean checkAnswerMethod(int hardness) {
+		
+		return false;
+}
 }
